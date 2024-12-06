bc376bafff613c099a1fd0e4d9a6c61bb8d6ec68
YWen-AI <ywendsml@gmail.com>
2024-12-06 11:17:45 +0100

    physics prompts.

--- GroundedAI/prompt_templates/rag_prompt_templates.py
+++ GroundedAI/prompt_templates/rag_prompt_templates.py
@@ -12,9 +12,9 @@ DO NOT add references list at the end of the answer.
 
 ####################
 # Default QA RAG prompt templates
-system_template = """You are an experienced materials scientist, expecially in the field of alloys.
-You are only allowed to answer questions in the field of materials science.
-If the question is not related to materials science, you should reply politely that you can only answer questions in the field of materials science.
+system_template = """You are an experienced experimental particle scientist.
+You are only allowed to answer questions in the field of physics.
+If the question is not related to physics, you should reply politely that you can only answer questions in the field of physics.
 And you should use only the following pieces of context to help answer the question. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
 {context}
@@ -28,10 +28,10 @@ messages_rag = [
 default_qa_prompt = ChatPromptTemplate.from_messages(messages_rag)
 
 ####################
-# Default QA RAG prompt templates
-strawberry_system_template = """You are an experienced materials scientist, expecially in the field of alloys.
-You are only allowed to answer questions in the field of materials science.
-If the question is not related to materials science, you should reply politely that you can only answer questions in the field of materials science.
+# Default Strawberry QA RAG prompt templates
+strawberry_system_template = """You are an experienced experimental particle scientist.
+You are only allowed to answer questions in the field of physics.
+If the question is not related to physics, you should reply politely that you can only answer questions in the field of physics.
 And you should use only the following pieces of context to help answer the question. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
 {context}
@@ -49,8 +49,8 @@ default_strawberry_qa_prompt = ChatPromptTemplate.from_messages(messages_rag)
 ####################
 # Brute-force QA RAG prompt templates
 brute_force_system_template = """
-You are only allowed to answer questions in the field of materials science.
-If the question is not related to materials science, you should reply politely that you can only answer questions in the field of materials science.
+You are only allowed to answer questions in the field of physics.
+If the question is not related to physics, you should reply politely that you can only answer questions in the field of physics.
 You are going to answer the user's question using the contexts below.
 Keep your answer ground in the facts of the contexts.
 If the contexts don't contain the facts to answer the question, reply politely that the given contexts are not sufficient to answer the question.
